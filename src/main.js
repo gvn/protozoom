ae36b5a0f3f204a8d968adbee5e07cbd4886a05d
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-04-01 11:09:09 -0700

    embedding tiles

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+function generateLayout (width, height) {
+  var layout = [];
+
+  for (var y = 0; y < height; y++) {
+    layout.push([]);
+
+    for (var x = 0; x < width; x++) {
+      layout[y].push(Math.random() > 0.5 ? null : 1);
+    }
+  }
+
+  return layout;
+}
+
 var Tile = React.createClass({
   render: function () {
     return (
@@ -9,24 +23,41 @@ var Tile = React.createClass({
 var Slot = React.createClass({
   render: function () {
     return (
-      <div className="slot"></div>
+      <div className="slot">
+        {this.props.children}
+      </div>
     )
   }
 })
 
 var Grid = React.createClass({
   render: function () {
+
+    var nodes = [];
+
+    for (var y = 0; y < this.props.layout.length; y++) {
+      for (var x = 0; x < this.props.layout[0].length; x++) {
+        if (this.props.layout[x][y]) {
+          nodes.push(
+            <Slot x={x} y={y}>
+              <Tile/>
+            </Slot>
+          )
+        } else {
+          nodes.push(<Slot x={x} y={y} />)
+        }
+      }
+    }
+
     return (
       <div className="grid">
-        <Slot/><Slot/><Slot/><Slot/>
-        <Slot/><Slot/><Slot/><Slot/>
-        <Slot/><Slot/><Slot/><Slot/>
+        {nodes}
       </div>
     )
   }
 })
 
 React.render(
-  <Grid/>,
+  <Grid layout={ generateLayout(4,10) }/>,
   document.querySelector('#app')
 );
